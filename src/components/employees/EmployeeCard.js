bbf6b0cf4134305c42b3a8fab758c69a6227a0ef
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-24 15:45:07 -0600

    setup kennel.js created directories for animals, customers, employees, locations and render each to DOM with hard coded data

---
+++ src/components/employees/EmployeeCard.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Employees.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Employee Name:</h3>
+    </section>
+)
