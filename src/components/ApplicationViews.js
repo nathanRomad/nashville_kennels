a6bc4ddaedeef9370efa50851d650169c01ac759
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-26 15:14:34 -0600

    feat: navbar functional / ApplicationViews.js holds views for each navbar item

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalCard } from "./animal/AnimalCard"
+import { CustomerCard } from "./customers/CustomerCard"
+import { LocationsCard } from "./locations/LocationCard"
+import { EmployeeCard } from "./employees/EmployeeCard"
+
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route path="/animals">
+                <AnimalCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/customers */}
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/employees */}
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/locations */}
+            <Route path="/locations">
+                <LocationsCard />
+            </Route>
+
+        </>
+    )
+}
