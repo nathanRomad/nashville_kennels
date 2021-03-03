db75a7f16bc8c958ee5e367dca534ed5975e8d08
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-03 13:29:06 -0600

    fix: add animal button renders without CSS in correct position

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom" // import from libraries before your local modules
 import { AnimalContext } from "./AnimalProvider"
 import { LocationContext } from "../locations/LocationsProvider"
 import { CustomerContext } from "../customers/CustomerProvider"
@@ -11,6 +12,9 @@ export const AnimalList = () => {
   const { locations, getLocations } = useContext(LocationContext)
   const { customers, getCustomers } = useContext(CustomerContext)
 
+  // The useHistory hook let's us tell React which route we want to visit. We will use it to tell React to render the animal form component.
+  const history = useHistory()
+
   //useEffect - reach out to the world for animals API fetch call
   useEffect(() => {
     getLocations()
@@ -22,18 +26,22 @@ export const AnimalList = () => {
   // Re-render can invoke useEffect (depending on the dependency array values). This would result in an infinate loop.
 
   return (
-    <div className="animals">
-      {console.log("AnimalList: Render", animals)}
-      {
-        animals.map(animal => {
-          const owner = customers.find(c => c.id === animal.customerId)
-          const clinic = locations.find(l => l.id === animal.locationId)
-          return <AnimalCard key={animal.id}
-            location={clinic}
-            customer={owner}
-            animal={animal} />
-        })
-      }
-    </div>
+    <>
+        <button onClick={() => {history.push("/animals/create")}}>
+              Add Animal
+            </button>
+      <div className="animals">
+        {
+          animals.map(animal => {
+            const owner = customers.find(c => c.id === animal.customerId)
+            const clinic = locations.find(l => l.id === animal.locationId)
+            return <AnimalCard key={animal.id}
+              location={clinic}
+              customer={owner}
+              animal={animal} />
+          })
+        }
+      </div>
+    </>
   )
 }
